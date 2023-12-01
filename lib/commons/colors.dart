8f6ca5ca92422040994a7d2ea29c37390e474886
jvictordev1 <josevictorcruzrb@gmail.com>
2023-11-30 22:30:58 -0300

    aa

--- lib/commons/colors.dart
+++ lib/commons/colors.dart
@@ -5,9 +5,5 @@ class ColorPalette {
   static const Color lightGreen = Color.fromARGB(255, 123, 166, 138);
   static const Color textColor = Color.fromARGB(255, 0, 0, 0);
   static const Color grey = Color.fromARGB(255, 217, 217, 217);
-<<<<<<< HEAD
   static const Color textFieldBg = Color.fromARGB(255, 240, 240, 240);
 }
-=======
-}
->>>>>>> c454e812567a9c15fa59216bb383a95976dfdd80
