06cf2d4305f773a8d38c69e693f22cee0d821a6d
Cauã Tavares <tavarescauac@gmail.com>
2024-02-07 11:12:19 -0300

    style: adicionando componente hint

---
+++ src/components/hint-question.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./ui/button";
+
+export function HintQuestion() {
+    return (
+        <div className="mt-3 sm:mt-0 sm:ml-3">
+			<Button variant="secondary">?</Button>
+		</div>
+    );
+}
