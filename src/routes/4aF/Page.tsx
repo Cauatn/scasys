41720e9774be749e34324f066dc1f659d20b437d
jvictordev1 <josevictorcruzrb@gmail.com>
2024-02-06 22:50:57 -0300

    feat: inventario/1,2,3 repaginadas e conexao de rotas.

--- src/routes/4aF/Page.tsx
+++ src/routes/4aF/Page.tsx
@@ -1,8 +1,6 @@
+import { ItemsTable } from "@/components/Items-table";
 import { Button } from "@/components/ui/button";
 
-import { Link } from "react-router-dom";
-
-import { ItemsTable } from "@/components/Items-table";
 import {
   Select,
   SelectContent,
@@ -11,36 +9,41 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { useItemContext } from "@/context/ItemsContext";
+import { Link } from "react-router-dom";
 
 function FouraF() {
   const { setItemFase } = useItemContext();
 
   return (
-    <section className="h-screen mx-auto max-w-4xl">
-      <h1 className="text-2xl font-bold mb-6">Fase de Inventário</h1>
-      <div className="justify-between flex">
-        <div className="w-[40%] flex flex-col gap-5">
-          <Select onValueChange={(e) => setItemFase(e)}>
-            <SelectTrigger id="phase-select">
-              <SelectValue placeholder="selecione aqui" />
-            </SelectTrigger>
-            <SelectContent position="popper">
-              <SelectItem value="inicial">Inicial</SelectItem>
-              <SelectItem value="intermediaria">Intermediária</SelectItem>
-              <SelectItem value="final">Final</SelectItem>
-            </SelectContent>
-          </Select>
+    <>
+      <div className="flex justify-center">
+        <div className="flex flex-col gap-5 space-y-4 w-1/2">
+          <h1 className="text-2xl font-bold">Fase de Inventário</h1>
+          <div className="max-w-[300px]">
+            <Select onValueChange={(e) => setItemFase(e)}>
+              <SelectTrigger id="phase-select">
+                <SelectValue placeholder="selecione aqui" />
+              </SelectTrigger>
+              <SelectContent position="popper">
+                <SelectItem value="inicial">Inicial</SelectItem>
+                <SelectItem value="intermediaria">Intermediária</SelectItem>
+                <SelectItem value="final">Final</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
           <div className="flex items-center space-x-4">
             <p className="text-sm">Adicionar fase intermediaria?</p>
             <Button className="bg-green-500 text-white">+</Button>
           </div>
-          <Link to={"/inventario/2"} className="justify-self-end md:col-span-1">
-            <Button className="bg-green-400 w-fit ">Próximo</Button>
-          </Link>
+          <ItemsTable />
         </div>
-        <ItemsTable />
       </div>
-    </section>
+      <div className="flex justify-end">
+        <Link to={"/inventario/2"} className="flex justify-end">
+          <Button className="bg-green-400 w-44">Próximo</Button>
+        </Link>
+      </div>
+    </>
   );
 }
 
--- src/routes/6aETP/Page.tsx
+++ src/routes/6aETP/Page.tsx
@@ -7,50 +7,47 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
 export function SixaETP() {
-	const { setItemEspecificidade, setItemNome } = useItemContext();
+  const { setItemEspecificidade, setItemNome } = useItemContext();
 
-	return (
-		<div className="mx-auto max-w-4xl bg-white p-6 shadow-lg space-y-6">
-			<div className="flex flex-col w-full gap-4">
-				<h1 className="text-2xl font-bold w-full"> Fase de Inventario</h1>
-				<div className="flex items-center justify-between gap-4 mt-4 mb-4">
-					<div className="flex flex-col space-y-2">
-						<Label htmlFor="especificidade" className="pl-2">
-							Especificidade :
-						</Label>
-						<Input
-							id="especificidade"
-							placeholder="Especificidade"
-							onChange={(event) => setItemEspecificidade(event.target.value)}
-						/>
-					</div>
-					<div className="flex flex-col space-y-2">
-						<Label htmlFor="Item" className="pl-2">
-							Item :
-						</Label>
-
-						<Input
-							id="Item"
-							placeholder="Item"
-							onChange={(event) => setItemNome(event.target.value)}
-						/>
-					</div>
-					<div className="flex flex-col space-y-2">
-						<Label htmlFor="Formula" className="pl-2">
-							Formula Química :
-						</Label>
-						<Input id="Formula" placeholder="Formula" />
-					</div>
-				</div>
-				<div className="flex justify-end">
-					<Link to={"/inventario=4"}>
-						<Button className="bg-green-400">Proxima</Button>
-					</Link>
-				</div>
-			</div>
-			<div>
-				<ItemsTable />
-			</div>
-		</div>
-	);
+  return (
+    <>
+      <div className="flex justify-center">
+        <div className="flex flex-col gap-5 w-1/2 space-y-4">
+          <h1 className="text-2xl font-bold w-full"> Fase de Inventário</h1>
+          <div className="flex items-center justify-between gap-4 mt-4 mb-4">
+            <div className="flex flex-wrap justify-between w-full gap-5">
+              <div>
+                <Label htmlFor="especificidade">Especificidade:</Label>
+                <Input
+                  id="especificidade"
+                  placeholder="Especificidade"
+                  onChange={(event) =>
+                    setItemEspecificidade(event.target.value)
+                  }
+                />
+              </div>
+              <div>
+                <Label htmlFor="Item">Item:</Label>
+                <Input
+                  id="Item"
+                  placeholder="Item"
+                  onChange={(event) => setItemNome(event.target.value)}
+                />
+              </div>
+              <div>
+                <Label htmlFor="Formula">Formula Química:</Label>
+                <Input id="Formula" placeholder="Formula" />
+              </div>
+            </div>
+          </div>
+          <ItemsTable />
+        </div>
+      </div>
+      <div className="flex justify-end">
+        <Link to={"/inventario/4"} className="flex justify-end">
+          <Button className="bg-green-400 w-44">Próximo</Button>
+        </Link>
+      </div>
+    </>
+  );
 }
