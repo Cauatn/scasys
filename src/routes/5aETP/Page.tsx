53a86088ddafc32a05e9c519153b58785deb9387
Cauã Tavares <tavarescauac@gmail.com>
2024-02-10 09:52:26 -0300

    feat: validação ao(s) form(s) e criação de rota(s)

--- src/routes/5aETP/Page.tsx
+++ src/routes/5aETP/Page.tsx
@@ -1,87 +1,85 @@
-import { Button } from '@/components/ui/button'
-import { useItemContext } from '@/context/ItemsContext'
+import { Button } from "@/components/ui/button"
+import { useItemContext } from "@/context/ItemsContext"
 
-import { ItemsTable } from '@/components/Items-table'
-import { Input } from '@/components/ui/input'
-import { PlusIcon } from '@radix-ui/react-icons'
+import { ItemsTable } from "@/components/Items-table"
+import { Input } from "@/components/ui/input"
+import { PlusIcon } from "@radix-ui/react-icons"
 
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom"
 
-import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
 
 const InvSchema = z.object({
-    etapaNome: z.string(),
-    repeticoes: z.string().transform(Number),
+  etapaNome: z.string(),
+  repeticoes: z.string().transform(Number),
 })
 
 type InvSchema = z.infer<typeof InvSchema>
 
 function FiveaETP() {
-    const { setItemEtapa } = useItemContext()
+  const { setItemEtapa } = useItemContext()
 
-    const { register, handleSubmit } = useForm({
-        resolver: zodResolver(InvSchema),
-    })
+  const { register, handleSubmit } = useForm({
+    resolver: zodResolver(InvSchema),
+  })
 
-    const navigate = useNavigate()
+  const navigate = useNavigate()
 
-    const handleFormSubmit = (data: any) => {
-        console.log(data)
-        navigate('/inventory/3')
-    }
+  const handleFormSubmit = (data: any) => {
+    console.log(data)
+    navigate("/inventory/3")
+  }
 
-    return (
-        <form
-            className="flex h-full flex-col justify-between"
-            onSubmit={handleSubmit(handleFormSubmit)}
-        >
-            <div className="flex justify-center">
-                <div className="flex w-full max-w-full flex-col gap-5 space-y-4 xl:w-1/2">
-                    <h1 className="w-full text-2xl font-bold">
-                        Fase de Inventário
-                    </h1>
-                    <div className="max-w-[300px]">
-                        <Input
-                            placeholder="Forneça o nome para etapa"
-                            required
-                            {...register('etapaNome')}
-                        />
-                    </div>
-                    <div className="inline-flex space-x-2">
-                        <div className="inline-flex w-full flex-wrap content-center items-center justify-between gap-3">
-                            <div className="inline-flex items-center justify-start">
-                                <label className="w-44" htmlFor="rep">
-                                    Número de repetições :
-                                </label>
-                                <Input
-                                    id="rep"
-                                    type="number"
-                                    className="w-16"
-                                    min="1"
-                                    required
-                                    {...register('repeticoes')}
-                                />
-                            </div>
-                            <div className="space-x-2">
-                                <span>Adicionar etapa procedimental?</span>
-                                <Button className="rounded-full bg-green-500 p-3">
-                                    <PlusIcon className="p-0" />
-                                </Button>
-                            </div>
-                        </div>
-                    </div>
-                    <ItemsTable />
-                </div>
-            </div>
-            <div className="flex justify-end">
-                <Button className="w-44 bg-green-400" type="submit">
-                    Próximo
+  return (
+    <form
+      className="flex h-full flex-col justify-between"
+      onSubmit={handleSubmit(handleFormSubmit)}
+    >
+      <div className="flex justify-center">
+        <div className="flex w-full max-w-full flex-col gap-5 space-y-4 xl:w-1/2">
+          <h1 className="w-full text-2xl font-bold">Fase de Inventário</h1>
+          <div className="max-w-[300px]">
+            <Input
+              placeholder="Forneça o nome para etapa"
+              required
+              {...register("etapaNome")}
+            />
+          </div>
+          <div className="inline-flex space-x-2">
+            <div className="inline-flex w-full flex-wrap content-center items-center justify-between gap-3">
+              <div className="inline-flex items-center justify-start">
+                <label className="w-44" htmlFor="rep">
+                  Número de repetições :
+                </label>
+                <Input
+                  id="rep"
+                  type="number"
+                  className="w-16"
+                  min="1"
+                  required
+                  {...register("repeticoes")}
+                />
+              </div>
+              <div className="space-x-2">
+                <span>Adicionar etapa procedimental?</span>
+                <Button className="rounded-full bg-green-500 p-3">
+                  <PlusIcon className="p-0" />
                 </Button>
+              </div>
             </div>
-        </form>
-    )
+          </div>
+          <ItemsTable />
+        </div>
+      </div>
+      <div className="flex justify-end">
+        <Button className="w-44 bg-green-400" type="submit">
+          Próximo
+        </Button>
+      </div>
+    </form>
+  )
 }
 
 export { FiveaETP }
--- src/routes/6aETP/Page.tsx
+++ src/routes/6aETP/Page.tsx
@@ -1,92 +1,82 @@
-import { Button } from '@/components/ui/button'
-import { useItemContext } from '@/context/ItemsContext'
+import { Button } from "@/components/ui/button"
 
-import { ItemsTable } from '@/components/Items-table'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
+import { ItemsTable } from "@/components/Items-table"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom"
 
-import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
 
 const InvSchema = z.object({
-    especificidade: z.string(),
-    item: z.string(),
-    formula: z.string(),
+  especificidade: z.string(),
+  item: z.string(),
+  formula: z.string(),
 })
 
 type InvSchema = z.infer<typeof InvSchema>
 
 export function SixaETP() {
-    const { setItemEspecificidade, setItemNome } = useItemContext()
+  const { register, handleSubmit } = useForm({
+    resolver: zodResolver(InvSchema),
+  })
 
-    const { register, handleSubmit } = useForm({
-        resolver: zodResolver(InvSchema),
-    })
+  const navigate = useNavigate()
 
-    const navigate = useNavigate()
+  const handleFormSubmit = (data: any) => {
+    console.log(data)
+    navigate("/inventory/4")
+  }
 
-    const handleFormSubmit = (data: any) => {
-        console.log(data)
-        navigate('/inventory/4')
-    }
-
-    return (
-        <form
-            className="flex h-full flex-col justify-between"
-            onSubmit={handleSubmit(handleFormSubmit)}
-        >
-            <div className="flex justify-center">
-                <div className="flex w-full flex-col gap-5 space-y-4 xl:w-1/2">
-                    <h1 className="w-full text-2xl font-bold">
-                        {' '}
-                        Fase de Inventário
-                    </h1>
-                    <div className="mb-4 mt-4 flex items-center justify-between gap-4">
-                        <div className="flex w-full flex-wrap justify-between gap-5">
-                            <div>
-                                <Label htmlFor="especificidade">
-                                    Especificidade:
-                                </Label>
-                                <Input
-                                    id="especificidade"
-                                    placeholder="Especificidade"
-                                    {...register('especificidade')}
-                                    required
-                                />
-                            </div>
-                            <div>
-                                <Label htmlFor="Item">Item:</Label>
-                                <Input
-                                    id="Item"
-                                    placeholder="Item"
-                                    {...register('item')}
-                                    required
-                                />
-                            </div>
-                            <div>
-                                <Label htmlFor="Formula">
-                                    Formula Química:
-                                </Label>
-                                <Input
-                                    id="Formula"
-                                    placeholder="Formula"
-                                    {...register('formula')}
-                                    required
-                                />
-                            </div>
-                        </div>
-                    </div>
-                    <ItemsTable />
-                </div>
+  return (
+    <form
+      className="flex h-full flex-col justify-between"
+      onSubmit={handleSubmit(handleFormSubmit)}
+    >
+      <div className="flex justify-center">
+        <div className="flex w-full flex-col gap-5 space-y-4 xl:w-1/2">
+          <h1 className="w-full text-2xl font-bold"> Fase de Inventário</h1>
+          <div className="mb-4 mt-4 flex items-center justify-between gap-4">
+            <div className="flex w-full flex-wrap justify-between gap-5">
+              <div>
+                <Label htmlFor="especificidade">Especificidade:</Label>
+                <Input
+                  id="especificidade"
+                  placeholder="Especificidade"
+                  {...register("especificidade")}
+                  required
+                />
+              </div>
+              <div>
+                <Label htmlFor="Item">Item:</Label>
+                <Input
+                  id="Item"
+                  placeholder="Item"
+                  {...register("item")}
+                  required
+                />
+              </div>
+              <div>
+                <Label htmlFor="Formula">Formula Química:</Label>
+                <Input
+                  id="Formula"
+                  placeholder="Formula"
+                  {...register("formula")}
+                  required
+                />
+              </div>
             </div>
-            <div className="flex justify-end">
-                <Button className="w-44 bg-green-400" type="submit">
-                    Próximo
-                </Button>
-            </div>
-        </form>
-    )
+          </div>
+          <ItemsTable />
+        </div>
+      </div>
+      <div className="flex justify-end">
+        <Button className="w-44 bg-green-400" type="submit">
+          Próximo
+        </Button>
+      </div>
+    </form>
+  )
 }
