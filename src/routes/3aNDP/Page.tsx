f6fb0a498485b450f7c652f46fef806dd66b9c0f
Cauã Tavares <tavarescauac@gmail.com>
2024-02-10 09:47:01 -0300

    feat: validação ao(s) form(s) e criação de rota(s)

--- src/routes/3aNDP/Page.tsx
+++ src/routes/3aNDP/Page.tsx
@@ -1,116 +1,116 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
 import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+    Card,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card'
+
+import { Input } from '@/components/ui/input'
+
+import { Label } from '@radix-ui/react-dropdown-menu'
+
+import { useNavigate } from 'react-router-dom'
+
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Input } from "@/components/ui/input";
-import { useItemContext } from "@/context/ItemsContext";
-import { Label } from "@radix-ui/react-dropdown-menu";
-import { TriangleDownIcon, TriangleUpIcon } from "@radix-ui/react-icons";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectSeparator,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select'
+
+const NmSchema = z.object({
+    nomeProcedimento: z
+        .string()
+        .nonempty('O nome do procedimento é obrigatório'),
+    modoCalculo: z.string().nonempty('O modo de cálculo é obrigatório'),
+})
+
+type NmSchema = z.infer<typeof NmSchema>
 
 export default function TrheeaNDP() {
-  const { procedimentos, novoProcedimento } = useItemContext();
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedValue, setSelectedItem] = useState<string>("Selecione aqui");
-  const [procedimento, setProcedimento] = useState<string>("");
+    const { register, handleSubmit, setValue } = useForm({
+        resolver: zodResolver(NmSchema),
+    })
 
-  function setItem(e: Event) {
-    const target = e.target as HTMLDivElement;
-    setSelectedItem(target.textContent ?? "unknown");
-  }
-  return (
-    <>
-      <div className="flex flex-col h-full items-center justify-between w-full">
-        <div className="flex flex-col w-3/4 justify-center gap-10 sm:w-1/2">
-          <div>
-            <Label className="font-bold mb-3">
-              Forneça um nome para o procedimento, a ser avaliado:
-            </Label>
-            <Input
-              placeholder="Forneça um nome"
-              className="h-9 pl-2"
-              onChange={(event) => {
-                setProcedimento(event.target.value);
-              }}
-            />
-          </div>
-          <div className="flex flex-col gap-4 justify-between items-center sm:flex sm:flex-row">
-            <p>Escolha um modo de cálculo:</p>
-            <DropdownMenu>
-              <DropdownMenuTrigger
-                asChild
-                className="flex flex-row w-52 justify-between border rounded-md gap-1 pr-2 pl-2"
-              >
-                <Button variant="outline" onClick={() => setIsOpen(!isOpen)}>
-                  {selectedValue}
-                  {isOpen ? (
-                    <TriangleDownIcon className="size-6" />
-                  ) : (
-                    <TriangleUpIcon className="size-6" />
-                  )}
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-48">
-                <DropdownMenuLabel>Modos de calculo</DropdownMenuLabel>
-                <DropdownMenuGroup>
-                  <DropdownMenuItem onSelect={setItem}>
-                    Reducionista
-                  </DropdownMenuItem>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem onSelect={setItem}>Guiado</DropdownMenuItem>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem onSelect={setItem}>
-                    Expandido
-                  </DropdownMenuItem>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem onSelect={setItem}>
-                    Exaustivo
-                  </DropdownMenuItem>
-                </DropdownMenuGroup>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-          <div className="h-full flex flex-col justify-between">
-            <Card className="w-[100%]">
-              <CardHeader className="flex justify-start content-start items-start">
-                <CardTitle className="">Exemplos:</CardTitle>
-                <CardDescription className="">
-                  Síntese de polietileno - Reducionista
-                </CardDescription>
-                <CardDescription className="">
-                  Isolamento do ácido fórmico - Guiado
-                </CardDescription>
-              </CardHeader>
-            </Card>
-          </div>
-        </div>
-        <div className="flex justify-end">
-          <Link to={"/inventory/1"}>
-            <Button
-              className="bg-green-400 w-fit "
-              onClick={() => {
-                novoProcedimento(procedimento, selectedValue);
-                console.log(procedimentos);
-              }}
+    const navigate = useNavigate()
+
+    const handleFormSubmit = (data: any) => {
+        console.log(data)
+        navigate('/inventory')
+    }
+
+    return (
+        <>
+            <form
+                className="flex h-full w-full flex-col items-center justify-between"
+                onSubmit={handleSubmit(handleFormSubmit)}
             >
-              Salvar procedimento
-            </Button>
-          </Link>
-        </div>
-      </div>
-    </>
-  );
+                <div className="flex w-3/4 flex-col justify-center gap-10 sm:w-1/2">
+                    <div>
+                        <Label className="mb-3 font-bold">
+                            Forneça um nome para o procedimento, a ser avaliado:
+                        </Label>
+                        <Input
+                            placeholder="Forneça um nome"
+                            className="h-9 pl-2"
+                            {...register('nomeProcedimento')}
+                            required
+                        />
+                    </div>
+                    <div className="flex flex-col items-center justify-between gap-4 sm:flex sm:flex-row">
+                        <p>Escolha um modo de cálculo:</p>
+                        <Select
+                            onValueChange={(value) =>
+                                setValue('modoCalculo', value)
+                            }
+                            required
+                        >
+                            <SelectTrigger className="flex w-52 flex-row justify-between gap-1 rounded-md border pl-2 pr-2">
+                                <SelectValue placeholder="Selecione o composto" />
+                            </SelectTrigger>
+                            <SelectContent className="w-48">
+                                <SelectItem value="Reducionista">
+                                    Reducionista
+                                </SelectItem>
+                                <SelectSeparator />
+                                <SelectItem value="Guiado">Guiado</SelectItem>
+                                <SelectSeparator />
+                                <SelectItem value="Expandido">
+                                    Expandido
+                                </SelectItem>
+                                <SelectSeparator />
+                                <SelectItem value="Exaustivo">
+                                    Exaustivo
+                                </SelectItem>
+                            </SelectContent>
+                        </Select>
+                    </div>
+                    <div className="flex h-full flex-col justify-between">
+                        <Card className="w-full">
+                            <CardHeader className="flex content-start items-start justify-start">
+                                <CardTitle className="">Exemplos:</CardTitle>
+                                <CardDescription className="">
+                                    Síntese de polietileno - Reducionista
+                                </CardDescription>
+                                <CardDescription className="">
+                                    Isolamento do ácido fórmico - Guiado
+                                </CardDescription>
+                            </CardHeader>
+                        </Card>
+                    </div>
+                </div>
+                <div className="flex w-1/2  justify-end">
+                    <Button className="w-fit bg-green-400 " type="submit">
+                        Proximo
+                    </Button>
+                </div>
+            </form>
+        </>
+    )
 }
--- src/routes/4aF/Page.tsx
+++ src/routes/4aF/Page.tsx
@@ -1,51 +1,75 @@
-import { ItemsTable } from "@/components/Items-table";
-import { Button } from "@/components/ui/button";
+import { ItemsTable } from '@/components/Items-table'
+import { Button } from '@/components/ui/button'
 
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { useItemContext } from "@/context/ItemsContext";
-import { Link } from "react-router-dom";
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select'
 
-function FouraF() {
-  const { setItemFase } = useItemContext();
+import { useNavigate } from 'react-router-dom'
 
-  return (
-    <>
-      <div className="flex justify-center">
-        <div className="flex flex-col gap-5 space-y-4 max-w-full w-full xl:w-1/2">
-          <h1 className="text-2xl font-bold">Fase de Inventário</h1>
-          <div className="max-w-[300px]">
-            <Select onValueChange={(e) => setItemFase(e)}>
-              <SelectTrigger id="phase-select">
-                <SelectValue placeholder="selecione aqui" />
-              </SelectTrigger>
-              <SelectContent position="popper">
-                <SelectItem value="inicial">Inicial</SelectItem>
-                <SelectItem value="intermediaria">Intermediária</SelectItem>
-                <SelectItem value="final">Final</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-          <div className="flex items-center space-x-4">
-            <p className="text-sm">Adicionar fase intermediaria?</p>
-            <Button className="bg-green-500 text-white">+</Button>
-          </div>
-          <ItemsTable />
-        </div>
-      </div>
-      <div className="flex justify-end">
-        <Link to={"/inventory/2"} className="flex justify-end">
-          <Button className="bg-green-400 w-44">Próximo</Button>
-        </Link>
-      </div>
-    </>
-  );
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+
+const InvSchema = z.object({
+    pashe: z.string(),
+})
+
+type InvSchema = z.infer<typeof InvSchema>
+
+export default function FouraF() {
+    const { handleSubmit, setValue } = useForm({
+        resolver: zodResolver(InvSchema),
+    })
+
+    const navigate = useNavigate()
+
+    const handleFormSubmit = (data: any) => {
+        console.log(data)
+        navigate('/inventory/2')
+    }
+
+    return (
+        <form
+            className="flex h-full flex-col justify-between"
+            onSubmit={handleSubmit(handleFormSubmit)}
+        >
+            <div className="flex justify-center">
+                <div className="flex w-full max-w-full flex-col gap-5 space-y-4 xl:w-1/2">
+                    <h1 className="text-2xl font-bold">Fase de Inventário</h1>
+                    <div className="max-w-[300px]">
+                        <Select
+                            onValueChange={(value) => setValue('pashe', value)}
+                            required
+                        >
+                            <SelectTrigger id="phase-select">
+                                <SelectValue placeholder="selecione aqui" />
+                            </SelectTrigger>
+                            <SelectContent position="popper">
+                                <SelectItem value="inicial">Inicial</SelectItem>
+                                <SelectItem value="intermediaria">
+                                    Intermediária
+                                </SelectItem>
+                                <SelectItem value="final">Final</SelectItem>
+                            </SelectContent>
+                        </Select>
+                    </div>
+                    <div className="flex items-center space-x-4">
+                        <p className="text-sm">Adicionar fase intermediaria?</p>
+                        <Button className="bg-green-500 text-white">+</Button>
+                    </div>
+                    <ItemsTable />
+                </div>
+            </div>
+            <div className="flex justify-end">
+                <Button className="w-44 bg-green-400" type="submit">
+                    Próximo
+                </Button>
+            </div>
+        </form>
+    )
 }
-
-export { FouraF };
-
