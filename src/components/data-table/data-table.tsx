2f92cecd3ade6397da4b0f94c4b8c087db368f3f
Cauã Tavares <tavarescauac@gmail.com>
2024-04-24 16:48:04 -0300

    Update adding some useful components

--- src/components/data-table/data-table.tsx
+++ src/components/data-table/data-table.tsx
@@ -45,6 +45,7 @@ export function DataTable<TData, TValue>({
   )
 
   useEffect(() => {
+    console.log("selectedRowsData", selectedRowsData)
     setSelectedRows(selectedRowsData)
   }, [selectedRowsData])
 
@@ -77,7 +78,7 @@ export function DataTable<TData, TValue>({
                 data-state={row.getIsSelected() && "selected"}
               >
                 {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
+                  <TableCell key={cell.id} className="font-medium">
                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
                   </TableCell>
                 ))}
@@ -85,7 +86,10 @@ export function DataTable<TData, TValue>({
             ))
           ) : (
             <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
+              <TableCell
+                colSpan={columns.length}
+                className="h-24 text-center font-medium"
+              >
                 No results.
               </TableCell>
             </TableRow>
