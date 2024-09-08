ba201bfdb949d99d0541aa7e3b08e98f00b782b5
cauatn <tavarescauac@gmail.com>
2024-09-08 11:17:51 -0300

    feat: form submission working correctly in 4a and 7a pages

--- src/pages/4A/Page.tsx
+++ src/pages/4A/Page.tsx
@@ -9,7 +9,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import Experiment from "@/context/experiment";
 
 import { useNavigate } from "react-router-dom";
@@ -24,13 +24,22 @@ export default function FourPage() {
 
   const navigate = useNavigate();
 
+  function handleSubmit(event: FormEvent) {
+    addInventoryPhase({ name: selectedPhase, steps: [] });
+    buffer.set("lastPhase", selectedPhase);
+    navigate("/app/5a");
+  }
+
   useEffect(() => {
     console.log(inventory);
   }, [inventory]);
 
   return (
     <>
-      <form className="flex flex-col w-full max-w-5xl mx-auto h-full">
+      <form
+        className="flex flex-col w-full max-w-5xl mx-auto h-full"
+        onSubmit={handleSubmit}
+      >
         <div className="flex flex-row justify-between w-full my-20">
           <Card className="rounded-none flex justify-center items-center max-w-[400px] max-h-[150px] w-full h-full p-7">
             <CardContent className="w-full h-full">
@@ -39,8 +48,7 @@ export default function FourPage() {
               </label>
               <Select
                 onValueChange={(value) => {
-                  addInventoryPhase({ name: value, steps: [] });
-                  buffer.set("lastPhase", value);
+                  setSelectedPhase(value);
                 }}
               >
                 <SelectTrigger className="w-full rounded-none border-black">
@@ -76,10 +84,7 @@ export default function FourPage() {
           )}
         </div>
         <div className="flex justify-end w-full">
-          <Button
-            className="bg-emerald-600"
-            onClick={() => navigate("/app/5a")}
-          >
+          <Button className="bg-emerald-600" type="submit">
             Proximo
           </Button>
         </div>
--- src/pages/7A/Page.tsx
+++ src/pages/7A/Page.tsx
@@ -1,14 +1,6 @@
 import { Input } from "@/components/ui/input";
-import { useSidebarToggle } from "@/hooks/use-side-bar-toggle";
-import { useStore } from "zustand";
 import { Card, CardContent } from "@/components/ui/card";
 import { FormEvent, useEffect, useState } from "react";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-  DropdownMenuItem,
-} from "@/components/ui/dropdown-menu";
 import {
   Select,
   SelectTrigger,
@@ -18,9 +10,19 @@ import {
 } from "@/components/ui/select";
 import { DataTable } from "@/components/items/data-table";
 import Experiment from "@/context/experiment";
-import { Button } from "@/components/ui/button";
-
 import { buffer } from "@/context/buffer";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { cn } from "@/lib/utils";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@/components/ui/button";
 
 export default function SevenPage() {
   const addItemQuantity = Experiment((state) => state.addItemQuantity);
@@ -28,17 +30,23 @@ export default function SevenPage() {
 
   const inventory = Experiment((state) => state.inventory);
 
+  const navigate = useNavigate();
+
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
 
+    addData();
+  };
+
+  function addData() {
     addItemQuantity(
-      buffer.get("lastPhase"),
       buffer.get("lastStep"),
+      buffer.get("lastPhase"),
       buffer.get("lastItem"),
       [1, 2, 3],
       observation
     );
-  };
+  }
 
   useEffect(() => {
     console.log(inventory);
@@ -97,10 +105,83 @@ export default function SevenPage() {
           </Card>
         </div>
         <DataTable />
-        <div className="flex justify-end w-full">
-          <Button className="bg-emerald-600" type="submit">
-            Proximo
+        <div className="flex justify-end w-full space-x-2">
+          <Button className="" type="submit" variant="outline">
+            Não
           </Button>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button className="bg-emerald-600">Sim</Button>
+            </DialogTrigger>
+            <DialogContent className={cn("rounded-none")}>
+              <DialogHeader>
+                <DialogTitle>
+                  Escolha aonde deseja adicionar o novo item
+                </DialogTitle>
+                <DialogDescription>
+                  Make changes to your profile here. Click save when you're
+                  done.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="grid gap-4 py-4">
+                <Card className="rounded-none flex justify-center items-center max-w-[400px] max-h-[150px] w-full p-4">
+                  <CardContent className="w-full space-y-2">
+                    <div className="flex flex-col">
+                      <label htmlFor="itemName">NOVA FASE</label>
+                    </div>
+                    <Button
+                      className="inline-flex space-x-2 items-center rounded-none w-full bg-slate-100 font-semibold"
+                      variant="ghost"
+                      type="submit"
+                      onClick={() => {
+                        addData();
+                        navigate("/app/4a");
+                      }}
+                    >
+                      ADICIONAR NOVA FASE
+                    </Button>
+                  </CardContent>
+                </Card>
+                <Card className="rounded-none flex justify-center items-center max-w-[400px] max-h-[150px] w-full p-4">
+                  <CardContent className="w-full space-y-2">
+                    <div className="flex flex-col">
+                      <label htmlFor="itemName">NOVA ETAPA</label>
+                    </div>
+                    <Button
+                      className="inline-flex space-x-2 items-center rounded-none w-full bg-slate-100 font-semibold"
+                      variant="ghost"
+                      onClick={() => {
+                        addData();
+                        navigate("/app/5a");
+                      }}
+                    >
+                      ADICIONAR NOVA ETAPA
+                    </Button>
+                  </CardContent>
+                </Card>
+                <Card className="rounded-none flex justify-center items-center max-w-[400px] max-h-[150px] w-full p-4">
+                  <CardContent className="w-full space-y-2">
+                    <div className="flex flex-col">
+                      <label htmlFor="itemName">MESMO ESTADO</label>
+                    </div>
+                    <Button
+                      className="inline-flex space-x-2 items-center rounded-none w-full bg-slate-100 font-semibold"
+                      variant="ghost"
+                      onClick={() => {
+                        addData();
+                        navigate("/app/6a");
+                      }}
+                    >
+                      ADICIONAR SOMENTE NOVO ITEM
+                    </Button>
+                  </CardContent>
+                </Card>
+              </div>
+              <DialogFooter>
+                {/* <Button type="submit">Save changes</Button> */}
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </div>
       </form>
     </>
