92c7c7b8be128104005b77cb84e89771d65db327
Gustavo Souza <gustavosouza1688@gmail.com>
2024-09-29 14:00:42 -0300

    FEAT: Add toaster component to the layout of the app

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import { Sidebar } from "./sidebar/Sidebar";
 import { Navbar } from "./navbar";
 import { Outlet } from "react-router-dom";
+import { Toaster } from "./ui/toaster";
 
 function Layout() {
   const sidebar = useStore(useSidebarToggle, (state) => state);
@@ -15,10 +16,11 @@ function Layout() {
       <Sidebar />
       <main
         className={cn(
-          "min-h-[calc(100vh)] bg-gray-50 dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300 max-w-screen flex flex-col overflow-x-hidden justify-start",
+          "min-h-[calc(100vh)] bg-gray-100 dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300 max-w-screen flex flex-col overflow-x-hidden justify-start",
           sidebar?.isOpen === false ? "lg:ml-[90px]" : "lg:ml-72"
         )}
       >
+        <Toaster />
         <Navbar title="Telas inicial" />
         <Outlet />
       </main>
