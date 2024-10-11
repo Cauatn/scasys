7934606fd35330e6e64bafd5f7df1092f23d6751
Gustavo Souza <gustavosouza1688@gmail.com>
2024-10-11 13:17:16 -0300

    FEAT: Add functionality to manage items inside containers

--- src/pages/8A/Page.tsx
+++ src/pages/8A/Page.tsx
@@ -11,14 +11,14 @@ import {
 } from "@/components/ui/select";
 import { useToast } from "@/hooks/use-toast";
 import { Trash2, Plus, Edit2 } from "lucide-react";
-import Bombona from "./components/Bombona";
+import Bombona, { Residuo } from "./components/Bombona";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
   PopoverClose,
 } from "@radix-ui/react-popover";
-import Experiment, { Item } from "@/context/experiment";
+// import Experiment from "@/context/experiment";
 import {
   Dialog,
   DialogContent,
@@ -29,6 +29,7 @@ import {
 } from "@/components/ui/dialog";
 import { DataTable } from "./components/Residuos/data-table";
 import { columns } from "./components/Residuos/columns";
+import { DialogClose } from "@radix-ui/react-dialog";
 
 export default function EigthPage() {
   const tableRef = useRef<any>(null);
@@ -37,23 +38,17 @@ export default function EigthPage() {
     {
       id: 1,
       title: "Bombona 1",
-      residuos: [
-        {
-          itemName: "residuo de ferro",
-          formula: "formula",
-          especificidade: "residuo",
-          quantitys: [],
-          observation: "obs",
-        },
-      ],
+      residuos: [],
     },
   ]);
   // const [selectedGroup, setselectedGroup] = useState("");
-  const getItemsByEspecificidade = Experiment(
-    (state) => state.getItemsByEspecificidade
-  );
+  // const getItemsByEspecificidade = Experiment(
+  //   (state) => state.getItemsByEspecificidade
+  // );
   // const residuos = getItemsByEspecificidade("residuo");
-  const residuos = [
+
+  // residuos de teste
+  const [residuos, setResiduos] = useState<Residuo[]>([
     {
       itemName: "residuo de ferro",
       formula: "formula",
@@ -75,17 +70,18 @@ export default function EigthPage() {
       quantitys: [],
       observation: "obs",
     },
-  ];
+  ]);
 
+  // hooks para edição do nome das bombonas
   const [editingId, setEditingId] = useState<number | null>(null);
   const editInputRef = useRef<HTMLInputElement>(null);
-
   useEffect(() => {
     if (editingId !== null && editInputRef.current) {
       editInputRef.current.focus();
     }
   }, [editingId]);
 
+  // função para adicionar uma bombona na página
   const addBombona = () => {
     const newId =
       bombonas.length > 0 ? Math.max(...bombonas.map((b) => b.id)) + 1 : 1;
@@ -95,17 +91,28 @@ export default function EigthPage() {
     ]);
   };
 
-  const deleteCard = (id: number) => {
+  // função para deletar uma bombona da página
+  const deleteBombona = (id: number) => {
     if (bombonas.length > 1) {
-      setBombonas(bombonas.filter((bombona) => bombona.id !== id));
-      toast({
-        variant: "default",
-        title: "Bombona deletada",
-        description:
-          "Bombona deletada com sucesso",
-      });
+      // Find the bombona to delete
+      const bombonaToDelete = bombonas.find((bombona) => bombona.id === id);
+
+      if (bombonaToDelete) {
+        // Remove the bombona reference from all its residuos
+        bombonaToDelete.residuos.forEach((residuo) => {
+          residuo.bombona = undefined; // Or null, depending on your preference
+        });
+
+        // Remove the bombona from the list of bombonas
+        setBombonas(bombonas.filter((bombona) => bombona.id !== id));
+
+        toast({
+          variant: "default",
+          title: "Bombona deletada",
+          description: "Bombona deletada com sucesso",
+        });
+      }
     } else {
-      // console.log("impossível ficar com menos de uma bombona ! :(");
       toast({
         variant: "default",
         title: "Impossível deletar",
@@ -115,23 +122,67 @@ export default function EigthPage() {
     }
   };
 
+  // função para iniciar a edição do nome de uma bombona
   const startEditing = (id: number) => {
     setEditingId(id);
   };
 
+  // função para terminar a edição do nome de uma bombona
   const finishEditing = (id: number, newTitle: string) => {
-    setBombonas(
-      bombonas.map((bombona) =>
+    setBombonas((prevBombonas) =>
+      prevBombonas.map((bombona) =>
         bombona.id === id ? { ...bombona, title: newTitle } : bombona
       )
     );
+
+    // atualizando o nome da bombona na tabela de residuos
+    setResiduos((prevResiduos) =>
+      prevResiduos.map((residuo) =>
+        residuo.bombona?.id === id
+          ? { ...residuo, bombona: { ...residuo.bombona, title: newTitle } }
+          : residuo
+      )
+    );
+
     setEditingId(null);
   };
 
   // Função para adicionar residuos à uma bombona
-  const addResiduosToBombona = (bombona: Bombona, residuos: Item[]) => {
-    console.log(residuos);
-  }
+  const addResiduosToBombona = (bombona: Bombona, residuos: Residuo[]) => {
+    if (residuos.length > 0) {
+      residuos.forEach((newResiduo) => {
+        // Verificar se o resíduo já existe em qualquer outra bombona
+        const existsInAnyBombona: boolean = bombonas.some((b) =>
+          b.residuos.some((residuo) => residuo.itemName === newResiduo.itemName)
+        );
+
+        if (!existsInAnyBombona) {
+          // Atualizar o estado das bombonas
+          setBombonas((prev) => {
+            return prev.map((b) => {
+              if (b.id === bombona.id) {
+                // Retornar uma nova bombona com o array de resíduos atualizado
+                return {
+                  ...b,
+                  residuos: [...b.residuos, newResiduo], // Adicionar o novo resíduo ao array
+                };
+              }
+              return b; // Retornar outras bombonas inalteradas
+            });
+          });
+          // Associar a bombona ao novo resíduo
+          newResiduo.bombona = bombona;
+        } else {
+          // throw new Error("Esse resíduo já existe em alguma bombona.");
+          toast({
+            variant: "destructive",
+            title: "Erro ao adicionar resíduo",
+            description: `O resíduo selecionado para adicionar já está presente em alguma bombona.`,
+          });
+        }
+      });
+    }
+  };
 
   return (
     <>
@@ -190,7 +241,7 @@ export default function EigthPage() {
                         <PopoverContent>
                           <Card className="border-gray-300">
                             <CardHeader>
-                              <h4 className="fonnClick={() => deleteCard(bombona.id)}t-medium leading-none">
+                              <h4 className="fonnClick={() => deleteBombona(bombona.id)}t-medium leading-none">
                                 Confirmar exclusão
                               </h4>
                             </CardHeader>
@@ -203,7 +254,7 @@ export default function EigthPage() {
                                   </p>
                                 </div>
                                 <div className="flex justify-end space-x-2">
-                                  <PopoverClose>
+                                  <PopoverClose asChild>
                                     <Button
                                       variant="outline"
                                       size="sm"
@@ -217,7 +268,7 @@ export default function EigthPage() {
                                     variant="destructive"
                                     size="sm"
                                     type="button"
-                                    onClick={() => deleteCard(bombona.id)}
+                                    onClick={() => deleteBombona(bombona.id)}
                                   >
                                     Excluir
                                   </Button>
@@ -240,11 +291,20 @@ export default function EigthPage() {
                             <SelectValue placeholder="Conjunto de resíduos" />
                           </SelectTrigger>
                           <SelectContent>
-                            {bombona.residuos.map((residuo, index) => (
-                              <SelectItem key={index} value={residuo.itemName}>
-                                {residuo.itemName}
+                            {bombona.residuos.length > 0 ? (
+                              bombona.residuos.map((residuo, index) => (
+                                <SelectItem
+                                  key={index}
+                                  value={residuo.itemName}
+                                >
+                                  {residuo.itemName}
+                                </SelectItem>
+                              ))
+                            ) : (
+                              <SelectItem value="no-residuo">
+                                Sem resíduos na bombona
                               </SelectItem>
-                            ))}
+                            )}
                           </SelectContent>
                         </Select>
                         <Input
@@ -286,12 +346,30 @@ export default function EigthPage() {
                               </DialogDescription>
                             </DialogHeader>
                             <div>
-                            <DataTable data={residuos} columns={columns} ref={tableRef}></DataTable>
+                              <DataTable
+                                data={residuos}
+                                columns={columns}
+                                ref={tableRef}
+                              ></DataTable>
                             </div>
-                            <div className="flex justify-end">
-                            <Button className="bg-emerald-600 w-52 " onClick={() => addResiduosToBombona(bombona, tableRef.current.getSelectedRowModel().rows.map((row: any) => row.original))}> Adicionar </Button>
-                            </div>
-                            
+                            <DialogClose asChild>
+                              <div className="flex justify-end">
+                                <Button
+                                  className="bg-emerald-600 w-52 "
+                                  onClick={() =>
+                                    addResiduosToBombona(
+                                      bombona,
+                                      tableRef.current
+                                        .getSelectedRowModel()
+                                        .rows.map((row: any) => row.original)
+                                    )
+                                  }
+                                >
+                                  {" "}
+                                  Adicionar{" "}
+                                </Button>
+                              </div>
+                            </DialogClose>
                           </DialogContent>
                         </Dialog>
                       </div>
@@ -317,17 +395,6 @@ export default function EigthPage() {
             </Button>
           </div>
         </form>
-
-        {/* <div>
-          <Card className="rounded-none w-fit ">
-            <CardContent className="p-4">
-              <Button type="button" className="bg-blue-500">
-                <Plus className="mr-2 h-4 w-4" />
-                Criar novo conjunto
-              </Button>
-            </CardContent>
-          </Card>
-        </div> */}
       </div>
     </>
   );
