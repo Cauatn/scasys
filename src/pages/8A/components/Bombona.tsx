6cd4f5f192a181d2afe0fe508fca1e626dc37973
Gustavo Souza <gustavosouza1688@gmail.com>
2024-10-28 15:18:47 -0300

    FEAT: Add ID to Residuo and add Relation between Bombona and Residuo

--- src/pages/8A/components/Bombona.tsx
+++ src/pages/8A/components/Bombona.tsx
@@ -1,44 +1,17 @@
 import { Item } from "@/context/experiment";
 
-interface Bombona {
+export interface Bombona {
   id: number;
   title: string;
-  residuos: Residuo[];
 }
 
 export interface Residuo extends Item {
-  bombona?: Bombona;
+  id: number;
   phaseName: string;
   stepName: string;
 }
 
-export function addResiduo(bombona: Bombona, newResiduo: Residuo): void {
-  const exists = bombona.residuos.some(
-    (item) => item.itemName === newResiduo.itemName
-  );
-
-  if (!exists) {
-    bombona.residuos.push(newResiduo);
-    newResiduo.bombona = bombona;
-  } else {
-    throw new Error("That residuo already exists in this bombona");
-  }
+export interface BombonaResiduoRelation {
+  bombonaId: number;
+  residuoId: number;
 }
-
-export function removeResiduo(bombona: Bombona, itemNameToRemove: string): void {
-  const index = bombona.residuos.findIndex(
-    (item) => item.itemName === itemNameToRemove
-  );
-
-  if (index !== -1) {
-    bombona.residuos.splice(index, 1);
-  } else {
-    throw new Error("Residou not found inside the bombona");
-  }
-}
-
-export function hasResiduo(bombona: Bombona, itemNameToCheck: string): boolean {
-    return bombona.residuos.some((item) => item.itemName === itemNameToCheck);
-  }
-
-export default Bombona;
